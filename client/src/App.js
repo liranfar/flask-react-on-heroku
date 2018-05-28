82306fc658436dae727234314c24c8d70f276927
liranfar <l.farage@gmail.com>
2018-05-28 16:51:19 +0300

    added simple async request

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (logMessage) => {
+    console.log(logMessage);
+    axios.get(process.env.PUBLIC_URL + '/users/ping', {
+                    // params :{
+                    //     dataType: 'json'
+                    // }
+                })
+                    .then(function (response) {
+                        console.log('SUCCESS', response)
+                    })
+                    .catch(function (error) {
+                        console.log('ERROR', error)
+                    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +35,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button
+          onClick={() =>
+            this.handleClick('Hello World!')
+          }
+        >
+        Click Me !
+        </button>
       </div>
     );
   }
